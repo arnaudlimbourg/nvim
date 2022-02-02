f9b2e9edc856303f862ce7beed13c0c0bd6ee7f2
Arnaud Limbourg <arnaud@limbourg.com>
2022-02-02 14:14:46 +0100

    Close chadtree when opening a file

--- lua/user/chadtree.lua
+++ lua/user/chadtree.lua
@@ -1,5 +1,6 @@
-local chadtree_settings = { 
-  ["keymap.primary"] = { "o", "<enter>" } 
+local chadtree_settings = {
+  ["keymap.primary"] = { "o", "<enter>" },
+  ["options.close_on_open"] = true
 }
 
 vim.api.nvim_set_var("chadtree_settings", chadtree_settings)
