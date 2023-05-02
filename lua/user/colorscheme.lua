9e9881f0e0bdd33e4293396205f6581380c752e1
Arnaud Limbourg <arnaud@limbourg.com>
2023-05-02 14:36:23 +0200

    Fix colorscheme syntax

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,9 +1,6 @@
-vim.cmd [[
-try
-  set background=dark
-  colorscheme nord
-catch /^Vim\%((\a\+)\)\=:E185/
-  colorscheme default
-  set background=dark
-endtry
-]]
+local colorscheme = "nord"
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  return
+end
