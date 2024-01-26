4f7060122689c0436921481a8a565bba07b2bc47
Arnaud Limbourg <arnaud@fitle.com>
2024-01-26 09:45:48 +0100

    Keep up with API change

--- lua/user/comment.lua
+++ lua/user/comment.lua
@@ -3,6 +3,8 @@ if not status_ok then
   return
 end
 
+vim.g.skip_ts_context_commentstring_module = true
+
 comment.setup {
   pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
 }
